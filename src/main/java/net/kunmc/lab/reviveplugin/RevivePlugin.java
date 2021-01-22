7d4e13dfbe717d30044afcc0a6f35af57f40cc22
Yu212 <41502287+Yu212@users.noreply.github.com>
2021-01-22 21:57:25 +0900

    Initial commit

---
+++ src/main/java/net/kunmc/lab/reviveplugin/RevivePlugin.java
@@ -0,0 +1,13 @@
+package net.kunmc.lab.reviveplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RevivePlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}

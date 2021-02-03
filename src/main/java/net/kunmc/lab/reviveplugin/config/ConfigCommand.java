c94cf42d8058bcd28dd97753cd0c56c7e9e4335e
Yu_212 <yu212.mc@gmail.com>
2021-02-03 19:23:13 +0900

    コンフィグ設定コマンドのメッセージを追加、tab補完を改善

--- src/main/java/net/kunmc/lab/reviveplugin/config/ConfigCommand.java
+++ src/main/java/net/kunmc/lab/reviveplugin/config/ConfigCommand.java
@@ -1,6 +1,8 @@
 package net.kunmc.lab.reviveplugin.config;
 
 import net.kunmc.lab.reviveplugin.RevivePlugin;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,7 +11,9 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ConfigCommand implements CommandExecutor, TabCompleter {
     @Override
@@ -17,27 +21,41 @@ public class ConfigCommand implements CommandExecutor, TabCompleter {
         ConfigManager configManager = RevivePlugin.getInstance().getConfigManager();
         if (args.length == 1 && args[0].equals("reload")) {
             configManager.load();
+            sender.sendMessage(ChatColor.GREEN + "[ReviveCommand] " + ChatColor.RESET + "コンフィグをリロードしました");
             return true;
         }
         if (args.length == 3 && args[0].equals("set")) {
             String path = args[1];
             String value = args[2];
-            return configManager.setConfig(path, value);
+            boolean result = configManager.setConfig(path, value);
+            if (result) {
+                sender.sendMessage(ChatColor.GREEN + "[ReviveCommand] " + ChatColor.RESET + path + "を" + value + "にセットしました");
+            } else {
+                sender.sendMessage(ChatColor.GREEN + "[ReviveCommand] " + ChatColor.RED + "コンフィグの設定に失敗しました");
+            }
+            return true;
         }
         return false;
     }
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
-        List<String> suggest = new ArrayList<>();
         if (args.length == 1) {
-            suggest.add("reload");
-            suggest.add("set");
+            return suggest(args[0], "reload", "set");
         } else if (args.length == 2 && args[0].equals("set")) {
-            suggest.addAll(ConfigManager.getConfigPaths());
+            return suggest(args[1], ConfigManager.getConfigPaths());
         } else {
             return null;
         }
-        return suggest;
+    }
+
+    private List<String> suggest(String arg, String... candidates) {
+        List<String> result = new ArrayList<>();
+        for (String candidate : candidates) {
+            if (candidate.startsWith(arg)) {
+                result.add(candidate);
+            }
+        }
+        return result;
     }
 }
